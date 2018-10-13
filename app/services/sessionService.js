fc50b1280d9b862a2d0ccc2750e04854ffadfe1a
fedea <fedeaperez@hotmail.com.ar>
2018-10-13 17:50:01 -0300

    no parse

--- app/services/sessionService.js
+++ app/services/sessionService.js
@@ -4,10 +4,10 @@ const database = firebase.database();
 module.exports = {
 	saveData: function(data) {
 		try {
-			var parsedData;
-			parsedData = JSON.parse(data);
-			var ref = database.ref('/session/' + parsedData.token_id);
-			return ref.set(parsedData);
+			/*var parsedData;
+			parsedData = JSON.parse(data);*/
+			var ref = database.ref('/session/' + data.Token_id);
+			return ref.set(data);
 		}
 		catch(err) {
 			throw new Error("Error al guardar los datos");
