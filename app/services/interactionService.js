0463e4d6c3153b1431d6095c6d6d94102066630a
fedea <fedeaperez@hotmail.com.ar>
2018-10-14 16:04:55 -0300

    log de interaccion

--- app/services/interactionService.js
+++ app/services/interactionService.js
@@ -3,7 +3,7 @@ const database = firebase.database();
 
 module.exports = {
     saveIntercation: function(req, res) {
-        var ref = database.ref('/interaction/' + req.path + '/' + Date.now());
+        var ref = database.ref('/interaction/' + Date.now());
         return ref.set({
             reqMethod : req.method,
             reqBody: req.body || "No body"
