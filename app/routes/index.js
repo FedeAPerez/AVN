8c94020e05a45b40147e508832bec3c1dc28c5a9
fedea <fedeaperez@hotmail.com.ar>
2018-10-28 01:45:10 -0300

    no patients

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,10 @@
 // routes/index.js
 const tokenRoutes = require('./tokenRoutes');
 const sessionRoutes = require('./sessionRoutes');
-const patientRoutes = require('./patientRoutes');
 const homeRoutes = require('./homeRoutes');
 
 module.exports = function(app) {
     tokenRoutes(app);
     sessionRoutes(app);
-    patientRoutes(app);
     homeRoutes(app);
 };
--- app/routes/patientRoutes.js
+++
@@ -1,5 +0,0 @@
-const patientController = require("../controllers/patientController");
-
-module.exports = function(app) {
-    app.get('/patient', patientController.getAllPatients);
-};
